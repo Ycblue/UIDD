81406acc4883ef27f9a0a364dd473216e58dc8a2
Ycblue <yuchialan@gmail.com>
2016-03-16 17:42:11 +0800

    new page

---
+++ page1/page1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#hiddendiv").hide();
+	$("#hiddentable").hide();
+	$("#checkimg").click(function(){
+		$("#hiddendiv").show();
+		$("#hiddentable").show();
+	});
+});
---
+++ page2/page2.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("#hiddendiv").hide();
+	$("#hiddentable").hide();
+	$("#wrongdiv").hide();
+	$("#checkimg").click(function(){
+	//	$("#hiddendiv").show();
+		$("#hiddentable").show();
+		$("#wrongdiv").show();
+	});
+});
